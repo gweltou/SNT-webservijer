fffd86019dfe8a9f184b9b38d0185f99da978c36
gweltou <gweltou@hotmail.com>
2020-01-13 09:44:52 +0100

    start script

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ python3 my_html_parser.py
 
 echo "executing: sudo python3 -m http.server 80 &"
 sudo python3 -m http.server 80 &
+sleep 2
 
 cd /home/pi/kaoz
 echo "executing: python3 servijer.py 8001 &"
