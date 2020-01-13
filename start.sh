c1608dde2a9b8776063a4cafbd29639294f9a75b
gweltou <gweltou@hotmail.com>
2020-01-13 09:43:30 +0100

    start script

--- start.sh
+++ start.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
 cd /home/pi/SNT-webservijer
+
+echo "executing: python3 my_html_parser.py"
 python3 my_html_parser.py
+
+echo "executing: sudo python3 -m http.server 80 &"
 sudo python3 -m http.server 80 &
 
 cd /home/pi/kaoz
+echo "executing: python3 servijer.py 8001 &"
 python3 servijer.py 8001 &
 
