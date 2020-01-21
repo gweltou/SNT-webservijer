ce066951639e5d60033fbca7a622e843fb4e6bcb
gweltou <gweltou@hotmail.com>
2020-01-21 09:05:08 +0100

    lauch script

--- start.sh
+++ start.sh
@@ -11,32 +11,32 @@ sleep 2
 
 
 echo "executing: python3 droopy.py -d pajennou/gwenn1 8000 &"
-python3 droopy.py -d pajennou/gwenn1 8000 &
-sleep 1
+python3 droopy.py -d "pajennou/gwenn1" 8000 &
+sleep 2
 
 echo "executing: python3 droopy.py -d pajennou/gwenn2 8001 &"
-python3 droopy.py -d pajennou/gwenn2 8001 &
-sleep 1
+python3 droopy.py -d "pajennou/gwenn2" 8001 &
+sleep 2
 
 echo "executing: python3 droopy.py -d pajennou/ruz1 8002 &"
-python3 droopy.py -d pajennou/ruz1 8002 &
-sleep 1
+python3 droopy.py -d "pajennou/ruz1" 8002 &
+sleep 2
 
 echo "executing: python3 droopy.py -d pajennou/ruz2 8003 &"
-python3 droopy.py -d pajennou/ruz2 8003 &
-sleep 1
+python3 droopy.py -d "pajennou/ruz2" 8003 &
+sleep 2
 
 echo "executing: python3 droopy.py -d pajennou/du 8004 &"
-python3 droopy.py -d pajennou/du 8004 &
-sleep 1
+python3 droopy.py -d "pajennou/du" 8004 &
+sleep 2
 
 echo "executing: python3 droopy.py -d pajennou/glas 8005 &"
-python3 droopy.py -d pajennou/glas 8005 &
-sleep 1
+python3 droopy.py -d "pajennou/glas" 8005 &
+sleep 2
 
 echo "executing: python3 droopy.py -d pajennou/kelennerien 8006 &"
-python3 droopy.py -d pajennou/kelennerien 8006 &
-sleep 1
+python3 droopy.py -d "pajennou/kelennerien" 8006 &
+sleep 2
 
 
 cd /home/pi/kaoz
